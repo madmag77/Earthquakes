f19d273be0bdf33b8a47a6eed3df0df7766e88b9
Artem Goncharov <artemswork@mail.ru>
2020-08-23 14:21:03 +0100

    Added map and mark for an earthquake on the map

--- Earthquakes/Features/Earthquake Details/EarthquakeDetailsViewController.swift
+++ Earthquakes/Features/Earthquake Details/EarthquakeDetailsViewController.swift
@@ -1,21 +1,44 @@
 import Foundation
-
+import MapKit
 import UIKit
 
 final class EarthquakeDetailsViewController: UIViewController {
+    @IBOutlet private var mapView: MKMapView!
+    
     var presenter: EarthquakeDetailsPresenter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mapView.delegate = self
+        navigationItem.backBarButtonItem?.title = "Back"
+        
         presenter?.viewDidLoad()
     }
-    
-
 }
 
 extension EarthquakeDetailsViewController: EarthquakeDetailsView {
     func updateView(with viewModel: EarthquakeDetailsViewmodel) {
+        mapView.centerToLocation(CLLocation(latitude:viewModel.lat, longitude:  viewModel.lon))
         
+        let mark = EarthquakeMark(
+            title: viewModel.earthquakeDescription,
+          coordinate: CLLocationCoordinate2D(latitude: viewModel.lat, longitude: viewModel.lon))
+        mapView.addAnnotation(mark)
     }
+}
+
+extension EarthquakeDetailsViewController: MKMapViewDelegate {
+    
+}
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 100000) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
 }
---
+++ Earthquakes/Features/Earthquake Details/EarthquakeMark.swift
@@ -0,0 +1,16 @@
+import MapKit
+
+final class EarthquakeMark: NSObject, MKAnnotation {
+  let title: String?
+  let coordinate: CLLocationCoordinate2D
+
+  init(
+    title: String?,
+    coordinate: CLLocationCoordinate2D
+  ) {
+    self.title = title
+    self.coordinate = coordinate
+
+    super.init()
+  }
+}
